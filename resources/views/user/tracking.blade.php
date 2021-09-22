6f5ffdebdbfc1f17436c801b01c48a1f3c7beacd
baukeyzh <baukey.zh@gmail.com>
2021-09-22 14:05:44 +0600

    v1.2

--- resources/views/user/tracking.blade.php
+++ resources/views/user/tracking.blade.php
@@ -6,7 +6,7 @@
                 <form action="{{route('info')}}" method="get">
                     <h3><label for="num">Введите номер HAWB:</label></h3>
                     @if(strlen($error_msg))
-                        <h4><label style="color:red" for="num">{{$error_msg}}</label></h4>
+                        <h4 style="margin-top: 14px"><label style="color:red" for="num">{{$error_msg}}</label></h4>
                     @endif
                     <input id="num" name="num" class="track-number-input" type="text" required>
                     <div class="block__content">
