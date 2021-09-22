a6c2068c008496cffa61285c369cf4365c130fc4
baukeyzh <baukey.zh@gmail.com>
2021-09-22 15:45:01 +0600

    v1.3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,8 +37,6 @@
     </div>
 
     <footer class="c-footer">
-        <div><a href="https://coreui.io">CoreUI</a> © 2020 creativeLabs.</div>
-        <div class="mfs-auto">Powered by&nbsp;<a href="https://coreui.io/">CoreUI</a></div>
     </footer>
 </div>
 
